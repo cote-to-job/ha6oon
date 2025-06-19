68e28cc8cd56b7c2a3191de853fb89291a2784df
ha6oon <hayun-lee@naver.com>
2025-06-19 23:54:43 +0900

    250617
    250617

---
+++ bj_1011_Fly me to the Alpha Centauri.py
@@ -0,0 +1,12 @@
+T = int(input())
+for _ in range(T):
+    x, y = map(int, input().split())
+    d = y - x
+    n = int(d**0.5)
+
+    if d == n**2:
+        print(2*n - 1)
+    elif d <= n**2 + n:
+        print(2*n)
+    else:
+        print(2*n + 1)
